411e15961dbd8a5936099d048d15d70411c1bc9c
unknown <katinaskornelijus@gmail.com>
2022-01-06 19:47:38 +0200

    Area ir Volume.

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -11,8 +11,13 @@ namespace TeamProject
             double izambine = 0;
 
             //Pirma Uzduotis
+            double radius = 6371;
+            earthArea = 4 * Math.PI * (radius * radius);
             //Antra Uzduotis
+            earthVolume = (4 / 3) * Math.PI * radius * radius;
+            
             //Trecia Uzduotis
+
             //Isvestis
 
         }
